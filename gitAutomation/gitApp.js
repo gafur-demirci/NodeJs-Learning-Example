f2c85f3499d3567560e045b7327e6ba1057598c1
gafur-demirci <gafur.gs.68@gmail.com>
2023-01-19 14:06:45 +0300

    acaip commit

--- gitAutomation/gitApp.js
+++ gitAutomation/gitApp.js
@@ -1,38 +1,5 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-class Post {
-    someFunction() { console.log('someFunc is run'); }
-}
-__decorate([
-    processOne(),
-    processTwo(),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", []),
-    __metadata("design:returntype", void 0)
-], Post.prototype, "someFunction", null);
-// decorator func lar sınıftan nesne üretmeden de çalışır.
-function processOne() {
-    console.log('processOne has run');
-    return function (target, propertyKey, descriptor) {
-        console.log('processOne has been called');
-    };
-}
-function processTwo() {
-    console.log('processTwo has run');
-    return function (target, propertyKey, descriptor) {
-        console.log('processTwo has been called');
-    };
-}
-var post = new Post();
-post.someFunction();
-//import { SimpleGit, CleanOptions, simpleGit } from "simple-git";
-//const git : SimpleGit = simpleGit().clean(CleanOptions.FORCE);
+Object.defineProperty(exports, "__esModule", { value: true });
+const simple_git_1 = require("simple-git");
+const git = (0, simple_git_1.simpleGit)().clean(simple_git_1.CleanOptions.FORCE);
 console.log("hello");
--- gitAutomation/gitApp.ts
+++ gitAutomation/gitApp.ts
@@ -2,4 +2,4 @@ import { SimpleGit, CleanOptions, simpleGit } from "simple-git";
 
 const git : SimpleGit = simpleGit().clean(CleanOptions.FORCE);
 
-console.log("hello");
+console.log("hello" + `https://open.spotify.com/track/12T19dCfYgZ7uOnIyaAPIi?si=bada498986634dea pls click :D `);
