821514b230e45c00a1168157979bb0bb8a05d6cf
gafur-demirci <gafur.gs.68@gmail.com>
2023-04-08 21:45:18 +0300

    Code refactoring

--- gitAutomation/gitPullTest.ts
+++ gitAutomation/gitPullTest.ts
@@ -1,25 +1,5 @@
-// import { PullResult, SimpleGit, simpleGit } from "simple-git";
 import { exec } from "child_process";
 
-// let file = "C:\\Users\\201054\\Desktop\\repos\\" + "authentication";
-
-// simpleGit("C:\\Users\\201054\\Desktop\\repos\\" + file)
-//    .exec(() => console.log('Starting pull...'))
-//    .pull((err, update) => {
-//       if (update && update.summary.changes) {
-//          require('child_process').exec('npm restart');
-//       }
-//    })
-//    .exec(() => console.log('pull doneeeeee.'));
-
-// exec(`cd ${file} && git pull`, (err,stdout,stderr) => {
-//    if(err) console.log(err + stderr);
-//    console.log("cd file: " +stdout);
-// })
-// exec('pwd', (err,stdout,stderr) => {
-//    if(err) console.log(err);
-//    console.log("pwd: " +stdout);
-// })
 export const pullFunc = async (file: String) => {
 
    //console.log("currrent folder is: " + file);
@@ -37,4 +17,3 @@ export const pullFunc = async (file: String) => {
    })
    
 }
-// when using async and optional chaining
