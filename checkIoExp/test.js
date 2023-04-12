2dea51d1c60812d58d3949ac03f3185b60509277
gafur-demirci <gafur.gs.68@gmail.com>
2023-04-12 15:23:54 +0300

    beginningZeros Solved

--- checkIoExp/test.js
+++ checkIoExp/test.js
@@ -2,7 +2,41 @@ const assert = require("assert");
 
 
 
+/* beginningZeros
+function beginningZeros(a) {
+    let zero = 0;
+    if (a[0] == 0) {
+        zero++
+        for (let index = 1; index < a.length; index++) {
+            const element = a[index];
+
+            if (element == 0) zero++
+            else return zero
+        }
+
+    }
+    else return 0
+    return zero
+}
+
+console.log("Example:");
+console.log(beginningZeros("10"));
+
+// These "asserts" are used for self-checking
+assert.strictEqual(beginningZeros("100"), 0);
+assert.strictEqual(beginningZeros("001"), 2);
+assert.strictEqual(beginningZeros("100100"), 0);
+assert.strictEqual(beginningZeros("001001"), 2);
+assert.strictEqual(beginningZeros("012345679"), 1);
+assert.strictEqual(beginningZeros("0000"), 4);
+
+console.log("Coding complete? Click 'Check Solution' to earn rewards!");
+ */
 /* changingDirection
+
+// best clear solution
+// var changingDirection = e => e.map((a,i,A)=>Math.sign(a-A[i-1])).filter(x=>x)
+//         .map((a,i,A)=>a-A[i-1]).filter(x=>x).length
 function changingDirection(elements) {
     let up = 0;
     let down = 0;
