04f06aba4fb11f966e3c90cebfc5449e43633132
gafur-demirci <gafur.gs.68@gmail.com>
2023-04-13 17:58:08 +0300

    correctSentence solved

--- checkIoExp/solve.ts
+++ checkIoExp/solve.ts
@@ -1,2 +1,37 @@
-console.log("test");
-console.log("test2");
+import assert from "assert";
+
+
+
+/* correctSentence
+function correctSentence(text: string): string {
+    return text[0].toUpperCase() + ((text[text.length - 1] !== ".") ? text.slice(1, text.length) + "." : text.slice(1, text.length));
+    //let start = text[0].toUpperCase()
+    //let end = text[text.length - 1]
+    //let miniText = text.slice(1, text.length)
+    //if(text[text.length-1] !== ".") text.slice(1,text.length) + "."
+    //let solve = text[0].toUpperCase() + ((text[text.length - 1] !== ".") ? text.slice(1, text.length) + "." : text.slice(1, text.length))
+}
+
+console.log("Example:");
+console.log(correctSentence("greetings, friends"));
+
+// These "asserts" are used for self-checking
+assert.strictEqual(
+    correctSentence("greetings, friends"),
+    "Greetings, friends."
+);
+assert.strictEqual(
+    correctSentence("Greetings, friends"),
+    "Greetings, friends."
+);
+assert.strictEqual(
+    correctSentence("Greetings, friends."),
+    "Greetings, friends."
+);
+assert.strictEqual(
+    correctSentence("greetings, friends."),
+    "Greetings, friends."
+);
+
+console.log("Coding complete? Click 'Check Solution' to earn rewards!");
+ */
